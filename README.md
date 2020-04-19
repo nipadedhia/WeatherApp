28936ed3f8098c41685ef865cdfdfc13f9b5e9bd
NCoder <nipa.dedhia@gmail.com>
2020-04-19 17:10:28 -0500

    add comments

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ## Weather-Dashboard
 
 Description:
-A simple web-app that allows you to search for a city and get the current weather and 5 day forecast.Displays date, temperature, humidity, wind speed, and UV index. Saves selected cities as buttons for ease of access.
+A simple web-app that allows you to search for a city and get the current weather and 5 day forecast.Displays date, temperature, humidity, wind speed, and UV index for current day. For 5 days, it displays date, temperature and humidity. It also saves selected cities previously searched and displays in left nav for ease of access. When the app is opened next time, it automatically loads data for the last city looked at.
+This app is using APIs from "Open Weather Map" for retrieving weather data. Using "Current Weather data" API for the current day weather data. Using "One Call API" for UV Index and 5 day weather data.
 
 ## How to use:
 
@@ -12,9 +13,9 @@ A simple web-app that allows you to search for a city and get the current weathe
 
 ## Tools Used
 
-OpenWeather's One Call API
+OpenWeatherMap's current Weather data API. Documentation available at https://openweathermap.org/current
+OpenWeatherMap's One Call API. Documentation available at https://openweathermap.org/api/one-call-api
 Bootstrap 4
 Moment.js
 JQuery framework
 Google font awesome
-
--- weather.js
+++ weather.js
@@ -5,96 +5,85 @@
 var navCity = [];
 var lastCity = localStorage.getItem("lastCity");
 
+//checking which last city was searched and rendering data for that city
 if (lastCity != null) {
   citySummary(lastCity);
 }
 
+//checking if local storage has any cities saved
 if (localStorage.getItem("leftNav") != null) {
   navCity = JSON.parse(localStorage.getItem("leftNav"));
-  //console.log("from local storage: " + navCity);
 }
 
 // declared function to get city when click on Search button
 var city;
 $("#submitCity").on("click", function () {
   city = $("#citySearchBox").val().trim();
-  //calling citySummary function
+  //calling citySummary function to populate the weather data for search city
   citySummary(city);
-  console.log(city);
 });
 
-// declared function to get city when hit enter key
-$("#input-form").on("submit", function () {
-  event.preventDefault();
-  city = $("#citySearchBox").val().trim();
-  //calling citySummary function
-  citySummary(city);
-  console.log(city);
-});
-
+//executing the weather API and retrieving current day weather data for the city passed as parameter
 function citySummary(city) {
   // Here we are building the URL we need to query the database
   var queryURL =
     "https://api.openweathermap.org/data/2.5/find?q=" +
     city +
     "&units=imperial&appid=4e468aa89195ad77b602b51a3d8f8672";
-  console.log(queryURL);
-  //console.log(city);
 
-  // We then created an AJAX call
+  // calling the weather API in this section
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
     lastCity = response.list[0].name;
     localStorage.setItem("lastCity", lastCity);
-    //console.log(localStorage);
     $("#citySummary").empty();
 
+    //declare the var for latitude and longitude which will be used for next API call to retrieve 5 day weather data
     var latitude = response.list[0].coord.lat;
     var longitude = response.list[0].coord.lon;
+    //calling five day weather data function
     fiveDay(latitude, longitude);
 
     //if array does not include last city then add it
     if (!navCity.includes(lastCity)) {
       navCity.push(lastCity);
     }
-
+    //saving the searched cities in the local storage
     localStorage.setItem("leftNav", JSON.stringify(navCity));
-
-    //call for addNav function
+    //call for addNav function to build the previously searched city list on left nav
     addNav();
-
-    //City Name
+    //rendering city name
     var cityName = $("<h3><strong>").text(lastCity);
 
-    //Current Date
+    //rendering current Date
     $(cityName).append(" (" + moment().format("MM/DD/YYYY") + ")");
 
-    //Image
+    //rendering weather icon
     $(cityName).append(
-      '<img src="http://openweathermap.org/img/wn/' +
+      '<img src="https://openweathermap.org/img/wn/' +
         response.list[0].weather[0].icon +
         '.png" />'
     );
-
+    //rendering temperature
     var temp = $("<br><p>").text(
       "Temperature: " + response.list[0].main.temp + " °F"
     );
+    //rendering humidity
     var humidity = $("<p>").text(
       "Humidity: " + response.list[0].main.humidity + "%"
     );
+    //rendering wind speed
     var windSpeed = $("<p>").text(
       "Wind Speed: " + response.list[0].wind.speed + " MPH"
     );
-
-    console.log("lon" + response.list[0].coord.lon);
-
+    //displaying all collected weather data
     $("#citySummary").append(cityName, temp, humidity, windSpeed);
   });
 }
 
+//calling "One Call API" to build and retrieve 5 day weather data based on the latitude and longitude passed as parameter
 function fiveDay(lat, lon) {
   var queryURL5Day =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -102,48 +91,44 @@ function fiveDay(lat, lon) {
     "&lon=" +
     lon +
     "&units=imperial&appid=4e468aa89195ad77b602b51a3d8f8672";
-
+  // calling the weather API in this section
   $.ajax({
     url: queryURL5Day,
     method: "GET",
   }).then(function (response5Day) {
-    console.log(response5Day);
-
     $("#fiveDaySum").empty();
-
     var uvi = $("<p>").text("UVI: " + response5Day.current.uvi);
     $("#citySummary").append(uvi);
 
     //for loop to display data for 5 days
-
     for (i = 1; i < 6; i++) {
-      //declared var for date and converting unix format to mm/dd/yy
-
+      //var for date and converting unix format to mm/dd/yy
       var fiveDate = $('<td class="px-2"> <p>').text(
         moment.unix(response5Day.daily[i].dt).format("MM/DD/YYYY")
       );
-      //declared var for weather icon
+      // var for weather icon
       var fiveIcon = $("<p>").append(
-        '<img src="http://openweathermap.org/img/wn/' +
+        '<img src="https://openweathermap.org/img/wn/' +
           response5Day.daily[i].weather[0].icon +
           '.png" />'
       );
-      //declared var for temperature
+      //var for temperature
       var fiveTemp = $("<p>").text(
         "Temp: " + response5Day.daily[i].temp.day + " °F"
       );
-      //declared var for humidity
+      //var for humidity
       var fiveHumidity = $("<p>").text(
         "Humidity: " + response5Day.daily[i].humidity + "%"
       );
       //
       $(fiveDate).append("<p>", fiveIcon, fiveTemp, fiveHumidity);
-      //display using .append five day data
+      //display five day data
       $("#fiveDaySum").append(fiveDate);
     }
   });
 }
 
+//searched city being displayed on left nav
 function addNav() {
   $("#citySearch").empty();
   for (var i = 0; i < navCity.length; i++) {
@@ -157,7 +142,6 @@ function addNav() {
 
     $("#navButton").on("click", function () {
       var citySearch = $(this).val();
-      console.log(citySearch);
       citySummary(citySearch);
     });
   }
